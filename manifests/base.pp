514ec700473a3169dc9ae03a38da9e8feb3de02d
Mario Anton <marioanton@users.noreply.github.com>
2016-12-13 13:11:25 +0100

    Update base.pp

--- manifests/base.pp
+++ manifests/base.pp
@@ -9,20 +9,54 @@
 #  include ipset
 #
 class ipset::base inherits ipset::params {
-
+  $startscript = "/usr/libexec/ipset/ipset.start-stop"
+   
+    case $::osfamily {
+    'RedHat': {
+        $service_path = $::operatingsystemmajrelease ? {
+            /(7)/   => '/lib/systemd/system',
+        }
+        $service_file = $::operatingsystemmajrelease ? {
+            /(7)/   => "ipset.service",
+        }
+    }
+  }
   # Main package
   package { $ipset::params::package:
     alias  => 'ipset',
     ensure => installed,
   }
 
-  # Custom script
+
+ 
   file { '/usr/local/sbin/ipset_from_file':
     owner  => 'root',
     group  => 'root',
     mode   => '0755',
     source => "puppet:///modules/${module_name}/ipset_from_file",
   }
+  
+   ($color == 'blue') and ($sound == 'loud')
+  
+  if ($::osfamily = 'RedHat') and ($operatingsystemrelease =~ /^7.*/  ) {
 
+    file { '/etc/ipset':
+      ensure => 'directory',
+    }
+
+    file { $service_file:
+      ensure  => 'present',
+      replace => 'no',
+      source  => 'puppet:///modules/ipset/ipset.service',
+      owner   => $user,
+    }
+
+    file { $startscript:
+      ensure  => 'present',
+      replace => 'no',
+      source  => 'puppet:///modules/ipset/ipset.start-stop',
+      owner   => $user,
+    }
+  } 
 }
 

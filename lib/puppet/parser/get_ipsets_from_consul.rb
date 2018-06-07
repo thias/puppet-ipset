ffb09e1e972e8036b14f5e02aecbb1f22fb2f4f5
Sava Varadzhakov <varadjakov@gmail.com>
2018-06-07 18:21:54 +0300

    Revert to functions that retrieve the ipsets from consul

--- lib/puppet/parser/get_ipsets_from_consul.rb
+++ lib/puppet/parser/get_ipsets_from_consul.rb
@@ -1,13 +1,25 @@
 module Puppet::Parser::Functions
-    newfunction(:crunch_conan_ipsets_data, :type => :rvalue) do |args|
+    newfunction(:get_ipsets_from_consul, :type => :rvalue) do |args|
        
+        require 'net/http'
         require 'json'
         require "base64"
-
-        rawipsets = args[0]
-
+        
+        # get url from args
+        url = args[0]
+        
+        uri = URI.parse(url)
+        response = Net::HTTP.get_response(uri)
+        
+        responseBodyParsed = ""
+        if response.code === "200"
+            responseBodyParsed = JSON.parse(response.body)    
+        else
+            raise response.message
+        end
+        
         # responses from consul API come as base64 encoded
-        rawIpsetsDecoded = Base64.decode64(rawipsets.first["Value"])
+        responseBodyDecoded = Base64.decode64(responseBodyParsed.first["Value"])
         
         # get parsed version of the config as a hash
         ipsets = JSON.parse(rawIpsetsDecoded)
